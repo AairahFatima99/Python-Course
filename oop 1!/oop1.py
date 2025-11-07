3bc612884632dc588872b473e659fb17a573244d
AairahFatima99 <aairah.fatima.ali@gmail.com>
2025-11-07 17:48:01 +0400

    oop

---
+++ oop 1!/oop1.py
@@ -0,0 +1,12 @@
+class fruit:
+
+# Constructor 
+ def __init__(self, name, color):
+    self.name = name
+    self.color = color
+
+
+# Object Creation
+apple = fruit("Apple","Red")
+# Calling function
+print(apple.name)
