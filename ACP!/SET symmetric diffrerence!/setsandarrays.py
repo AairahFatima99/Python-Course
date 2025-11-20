c476c74be5f34dfda337a940d0e3e02847bd09e4
AairahFatima99 <aairah.fatima.ali@gmail.com>
2025-11-20 20:21:57 +0400

    setandarray

---
+++ ACP!/SET symmetric diffrerence!/setsandarrays.py
@@ -0,0 +1,11 @@
+# we can make set from list
+my_set = set([1, 2, 3, 2])
+print(my_set, "\n")
+
+#remove a number from a set
+num_set = set([0, 1, 3, 4, 5])
+print("Original set:")
+print(num_set)
+num_set.pop()
+print("After removing the first element from the said set: ")
+print(num_set, "\n")
