0d9d9a1cbbf30e370233a15675978b327e5569eb
AairahFatima99 <aairah.fatima.ali@gmail.com>
2025-11-28 17:47:51 +0400

    abstration and poly

---
+++ Abstarction and Polymorphisum!/poly1.py
@@ -0,0 +1,34 @@
+# Class 1 
+class Pakistan():
+    def capital(self):
+        print("Islamabad is the capital of Pakistan")
+
+    def language(self):
+        print("Urdu is the most widely spoken language of Pakistan.")
+
+def type(self):
+    print("Pakistan is a devolped country.")
+
+# Class 2 
+class UAE():
+    def capital(self):
+        print("Abu Dhabi is the capital of UAE")
+    def language(self):
+        print("Arabic is the most widely spoken language of UAE.")
+def type(self):
+    print("UAE is a devolped country.")
+
+obj_pak = Pakistan()
+obj_uae = UAE()
+
+for country in (obj_pak, obj_uae):
+    country.capital()
+    country.language()
+    country.type()
+
+
+    
+ 
+
+
+
